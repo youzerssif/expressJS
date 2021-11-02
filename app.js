3228b80fb49aadb5e96afb3c155b613608f63e83
SAMAKE MOHAMED <39623776+youzerssif@users.noreply.github.com>
2021-11-02 14:42:34 +0000

    user auth

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const stuffRoutes = require('./routes/stuff');
+const userRoutes = require('./routes/user');
 const app = express();
 
 
@@ -23,5 +24,6 @@ app.use((req, res, next) => {
 
 app.use(express.json());
 app.use('/api/stuff', stuffRoutes);
+app.use('/api/auth', userRoutes);
 
 module.exports = app;
